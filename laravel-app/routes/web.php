908656b27d4e06fda2c95e158725f6251bf229ca
Jazib-Khan <jazib1khan@gmail.com>
2025-01-14 13:07:29 +0000

    Login Authorization, Logout

--- laravel-app/routes/web.php
+++ laravel-app/routes/web.php
@@ -7,7 +7,16 @@ use Illuminate\Support\Facades\Route;
 
 Route::view('/', 'home');
 
-Route::resource('jobs', JobController::class);
+/* Route::resource('jobs', JobController::class); */
+Route::controller(JobController::class)->group(function () {
+    Route::get('/jobs', 'index');
+    Route::get('/jobs/create', 'create');
+    Route::get('/jobs/{job}', 'show');
+    Route::post('/jobs', 'store');
+    Route::get('/jobs/{job}/edit', 'edit');
+    Route::patch('/jobs/{job}', 'update');
+    Route::delete('/jobs/{job}', 'destroy');
+});
 
 Route::view('/contact', 'contact');
 
@@ -17,3 +26,4 @@ Route::post('/register', [RegisteredUserController::class, 'store']);
 
 Route::get('/login', [SessionController::class, 'create']);
 Route::post('/login', [SessionController::class, 'store']);
+Route::post('/logout', [SessionController::class, 'destroy']);
