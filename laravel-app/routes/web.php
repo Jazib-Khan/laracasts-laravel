f27b02c9eb30fefae5f20c2e9f727e1966908a4b
Jazib-Khan <jazib1khan@gmail.com>
2025-01-14 16:03:56 +0000

    Authorization, Policies, Gates

--- laravel-app/routes/web.php
+++ laravel-app/routes/web.php
@@ -12,8 +12,12 @@ Route::controller(JobController::class)->group(function () {
     Route::get('/jobs', 'index');
     Route::get('/jobs/create', 'create');
     Route::get('/jobs/{job}', 'show');
-    Route::post('/jobs', 'store');
-    Route::get('/jobs/{job}/edit', 'edit');
+    Route::post('/jobs', 'store')->middleware('auth');
+
+    Route::get('/jobs/{job}/edit', 'edit')
+        ->middleware('auth')
+        ->can('edit', 'job');
+
     Route::patch('/jobs/{job}', 'update');
     Route::delete('/jobs/{job}', 'destroy');
 });
@@ -24,6 +28,6 @@ Route::view('/contact', 'contact');
 Route::get('/register', [RegisteredUserController::class, 'create']);
 Route::post('/register', [RegisteredUserController::class, 'store']);
 
-Route::get('/login', [SessionController::class, 'create']);
+Route::get('/login', [SessionController::class, 'create'])->name('login');
 Route::post('/login', [SessionController::class, 'store']);
 Route::post('/logout', [SessionController::class, 'destroy']);
